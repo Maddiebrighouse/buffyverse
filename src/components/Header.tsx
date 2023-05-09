23aca618ed7b1492a8e9cab481b674a350a67512
maddiebrighouse <madeleinebrighouse@gmail.com>
2023-05-09 21:03:04 +1200

    moved link to just logo, added cloud

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,21 +5,26 @@ import { Link } from "react-router-dom";
 const Header: React.FC = () => {
   return (
     <div className="mx-64 pb-96">
+      <img
+        className="absolute ml-32 w-96 left-96"
+        src="src/assets/header/moon.png"
+        alt="Buffy the Vampire Slayer"
+      />
+      <img
+        className="absolute w-1/3 opacity-50 top-40 mix-blend-lighten right-60"
+        src="https://www.pngmart.com/files/11/Storm-Cloud-PNG.png"
+        alt="Buffy the Vampire Slayer"
+      />
       <Link to="/">
         <img
-          className="absolute ml-32 w-96 left-96"
-          src="src/assets/header/moon.png"
-          alt="Buffy the Vampire Slayer"
-        />
-        <img
           className="absolute w-1/3 mt-20 ml-64"
-          src="src/assets/header/title-noclouds.png"
+          src="src/assets/header/title.png"
           alt="Buffy the Vampire Slayer"
         />
-        <div className="absolute top-4 right-6">
-          <Nav />
-        </div>
       </Link>
+      <div className="absolute top-4 right-6">
+        <Nav />
+      </div>
     </div>
   );
 };
