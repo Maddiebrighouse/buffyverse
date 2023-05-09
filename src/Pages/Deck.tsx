ba185b6dc11e6775a3ac98768eb257871c850c12
maddiebrighouse <madeleinebrighouse@gmail.com>
2023-05-09 20:41:56 +1200

    moved filter inside Deck

--- src/Pages/Deck.tsx
+++ src/Pages/Deck.tsx
@@ -1,11 +1,14 @@
 import Card from "../components/Card";
+import Filters from "../components/Filter";
+import { people } from "../people.json";
 
-const Deck = ({ people }) => {
+const Deck = () => {
   return (
     <div className="px-6 mx-auto text-center max-w-7xl lg:px-8">
+      <Filters />
       <ul
         role="list"
-        className="grid max-w-2xl grid-cols-1 gap-6 mx-auto mt-20 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3 lg:gap-x-20"
+        className="grid max-w-2xl grid-cols-1 gap-6 mx-auto sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3 lg:gap-x-20"
       >
         {people.map((person) => (
           <Card {...person} />
