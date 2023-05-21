959e0efb4bb915dabda6dc306d04e222731fb6da
maddiebrighouse <madeleinebrighouse@gmail.com>
2023-05-21 16:58:44 +1200

    Clean-up after rebase

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ import { loader } from "./components/Loader";
 import { Provider, createClient, cacheExchange, fetchExchange } from "urql";
 
 const client = createClient({
-  url: "http://localhost:3000/",
+  url: "http://localhost:8080/",
   exchanges: [cacheExchange, fetchExchange],
 });
 
--- src/Pages/Deck.tsx
+++ src/Pages/Deck.tsx
@@ -1,4 +1,3 @@
-// import axios from "axios";
 import { Link } from "react-router-dom";
 import Card from "../components/Card";
 import { useEffect, useState } from "react";
